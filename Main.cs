cbda319389106af7f89f64f70a5ecc29e92c00fa
Georgii Belozubov <soackerstar@gmail.com>
2024-11-24 19:58:01 +0700

    Signed-off-by: Georgii Belozubov <soackerstar@gmail.com>

---
+++ Main.cs
@@ -0,0 +1,11 @@
+﻿namespace leetcodecs
+{
+    class Leetcodecs
+    {
+        private static void Main()
+        {
+            //Solution1.Solution();
+            Solution2.Solution();
+        }
+    }
+}
---
+++ Solution2.cs
@@ -0,0 +1,12 @@
+﻿namespace leetcodecs
+{
+    public class Solution2
+    {
+        public static int Solution()
+        {
+            Console.WriteLine(1e9);
+
+            return 0;
+        }
+    }
+}
